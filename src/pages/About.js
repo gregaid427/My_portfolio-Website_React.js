e2df25170021cdf7709c07fe28192a790781bc07
Gregory Aidoo <gregaid427@gmail.com>
2022-11-05 12:01:43 +0000

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,7 +12,7 @@ export const About = () => {
           </div>
         </div>
       
-      <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
+      <div className="max-w-[1000px] w-full flex md:flex-row sm:flex-col gap-8 px-4">
         <div className="sm:text-right text-4xl font-bold  ">
           <p>Hi, i'm Greg, nice to meet you, please take a look around</p>
         </div>
