f519e37ca6a3f434c902f6b51ab9f2dc92f4f532
Gregory Aidoo <gregaid427@gmail.com>
2022-11-05 11:52:16 +0000

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,6 +4,7 @@ import React from 'react'
 
 export const Contact = () => {
     return (
+      <div>
       <div name='contact' className=' w-full h-screen bg-slate-600  flex justify-center items-center p-4'>
         <form action='' className='flex flex-col max-w-[600px] w-full'>
           <div className='pt-10' >
@@ -17,6 +18,7 @@ export const Contact = () => {
           <textarea className='my-4 p-2 bg-[#ccd]' rows={"10"} placeholder="Message" name='message'></textarea>
           <button className='text-white border-2 hover:bg-red-600 hoverborder-pink-500 px-4 py-3 my-8 mx-auto flex items-center'> Submit </button>
         </form>
+      </div>
       </div>
         )
       }
