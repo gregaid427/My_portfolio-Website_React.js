238bf6aa861db8ef881f5fe4c8e0355f62b228b6
Gregory Aidoo <gregaid427@gmail.com>
2022-11-05 11:53:49 +0000

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,7 +4,7 @@ import React from 'react'
 
 export const Contact = () => {
     return (
-      <div>
+      <div className='pt-10 pb-10'>
       <div name='contact' className=' w-full h-screen bg-slate-600  flex justify-center items-center p-4'>
         <form action='' className='flex flex-col max-w-[600px] w-full'>
           <div className='pt-10' >
