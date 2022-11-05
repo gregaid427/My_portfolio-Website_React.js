cd1331ad309e6a008720a2a695bc6efb4cc3b62c
Gregory Aidoo <gregaid427@gmail.com>
2022-11-05 11:38:17 +0000

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,9 +4,9 @@ import React from 'react'
 
 export const Contact = () => {
     return (
-      <div name='contact' className='mt-5 w-full h-screen bg-slate-600  flex justify-center items-center p-4'>
+      <div name='contact' className=' w-full h-screen bg-slate-600  flex justify-center items-center p-4'>
         <form action='' className='flex flex-col max-w-[600px] w-full'>
-          <div className='pb-8' >
+          <div className='pb-8 pt-8' >
           <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300 ' >Contact</p>
           <p className='text-gray-300 py-4' > Submit the form below or shoot me an email - gregoryd428@gmail.com</p>
           <p className='text-gray-300 py-4 text-2xl' >gregoryd428@gmail.com</p>
